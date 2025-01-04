8a3294c35748326a0e2975986365252f088e1299
massicottetyler <tyler@pseudocube.com>
2025-01-04 13:56:45 -0800

    added Game Events. added Audio Events

---
+++ Assets/Scripts/Events/AudioEvent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+namespace Events
+{
+    /// <summary>
+    /// Data for the <see cref="AudioEvent"/>.
+    /// </summary>
+    [Serializable]
+    public class AudioEventData : GameEventData
+    {
+        /// <summary>
+        /// Audio clip to play.
+        /// </summary>
+        [Tooltip("Audio clip source")]
+        [SerializeField]
+        public AudioClip clip;
+        
+        /// <summary>
+        /// Volume of the audio clip.
+        /// </summary>
+        [Tooltip("Playback volume")]
+        [Range(0.0f, 1.0f)]
+        [SerializeField]
+        public float volume = 1;
+        
+        /// <summary>
+        /// Pitch of the audio clip.
+        /// </summary>
+        [Tooltip("Playback pitch")]
+        [Range(0.0f, 3.0f)]
+        [SerializeField]
+        public float pitch = 1;
+        
+        /// <summary>
+        /// Whether the audio clip should loop.
+        /// </summary>
+        [Tooltip("Loop playback")]
+        [SerializeField]
+        public bool loop = false;
+    }
+    
+    /// <summary>
+    /// Event that plays audio.
+    /// </summary>
+    [CreateAssetMenu(fileName = "Data", menuName = "Game Events/Audio", order = 0)]
+    public class AudioEvent : GameEvent<AudioEventData>
+    {
+        /// <summary>
+        /// Event that is triggered when the <see cref="AudioEvent"/> is invoked.
+        /// </summary>
+        public static Action<AudioEventData> OnAudioEvent;
+        
+        protected override void OnInvoke(GameObject invoker = null)
+        {
+            base.OnInvoke(invoker);
+            OnAudioEvent?.Invoke(data);
+        }
+    }
+}
---
+++ Assets/Scripts/Events/GameEvent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace Events
+{
+    /// <summary>
+    /// Interface for GameEvents.
+    /// </summary>
+    public interface IGameEvent
+    {
+        /// <summary>
+        /// Invokes the GameEvent.
+        /// </summary>
+        /// <param name="invoker">GameObject that is invoking this event.</param>
+        void Invoke(GameObject invoker = null);
+    }
+
+    /// <summary>
+    /// Data for the GameEvent.
+    /// </summary>
+    [Serializable]
+    public class GameEventData
+    {
+        
+    }
+
+    /// <summary>
+    /// Base class for GameEvents.
+    /// </summary>
+    /// <typeparam name="T"><see cref="GameEventData"/> for the Event.</typeparam>
+    public abstract class GameEvent<T> : ScriptableObject, IGameEvent where T : GameEventData
+    {
+        /// <summary>
+        /// Data for the GameEvent.
+        /// </summary>
+        [SerializeField]
+        protected T data;
+        
+        public void Invoke(GameObject invoker = null)
+        {
+        }
+        
+        /// <summary>
+        /// Called when the <see cref="GameEvent"/> is invoked.
+        /// </summary>
+        /// <param name="invoker">GameObject that invoked this event.</param>
+        protected virtual void OnInvoke(GameObject invoker = null)
+        {
+            Debug.Log("GameEvent Invoked: " + this.name);
+        }
+    }
+}
