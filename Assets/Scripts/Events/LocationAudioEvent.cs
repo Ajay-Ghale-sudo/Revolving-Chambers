d3a1e7c4eb0afb983a11a409225b78cca5296cd4
Tyler Massicotte <Tyler@pseudocube.com>
2025-01-04 22:06:52 +0000

    Merged PR 164: Game Events
    added Game Events. added Audio Events
    
    Related work items: #289, #298

---
+++ Assets/Scripts/Events/LocationAudioEvent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace Events
+{
+    /// <summary>
+    /// Data for the <see cref="LocationAudioEvent"/>.
+    /// </summary>
+    [Serializable]
+    public class LocationAudioEventData : AudioEventData
+    {
+        /// <summary>
+        /// Whether to use the invokers position when playing the audio.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Play audio at the invokers position")]
+        public bool useInvokersPosition = true;
+        
+        /// <summary>
+        /// Location of the audio source.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Location to play the audio")]
+        public Vector3 location = Vector3.zero;
+    }
+    
+    /// <summary>
+    /// Event that plays audio at a specific location.
+    /// </summary>
+    [CreateAssetMenu(fileName = "data", menuName = "Game Events/Location Audio", order = 1)]
+    public class LocationAudioEvent : GameEvent<LocationAudioEventData>
+    {
+        /// <summary>
+        /// Event that is triggered when the <see cref="LocationAudioEvent"/> is invoked.
+        /// </summary>
+        public static Action<LocationAudioEventData> OnLocationAudioEvent;
+        
+        protected override void OnInvoke(GameObject invoker = null)
+        {
+            base.OnInvoke(invoker);
+            
+            data.location = data.useInvokersPosition && invoker ? invoker.transform.position : data.location;
+            OnLocationAudioEvent?.Invoke(data);
+        }
+    }
+}
