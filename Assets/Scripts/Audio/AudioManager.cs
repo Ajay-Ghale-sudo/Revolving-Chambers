8e512a1d57aa2dbb070cc4b034ef5d27ccc80936
Tyler Massicotte <Tyler@pseudocube.com>
2025-01-05 12:03:39 +0000

    Merged PR 169: added Singleton base class. added audio manager
    added Singleton base class. added audio manager
    
    Related work items: #277

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Events;
+using UnityEngine;
+using Utility;
+
+namespace Audio
+{
+    /// <summary>
+    /// Manager for audio.
+    /// </summary>
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioManager : Singleton<AudioManager> 
+    {
+        /// <summary>
+        /// The <see cref="AudioSource"/> attached to this GameObject.
+        /// </summary>
+        private AudioSource _audioSource;
+
+        /// <summary>
+        /// A separate AudioSource for playing ambient audio. Allows for independent volume/settings control 
+        /// </summary>
+        private AudioSource _ambientAudioSource;
+        
+        /// <summary>
+        /// The <see cref="AudioListener"/> attached to this GameObject. Used when there is no player audio listener.
+        /// </summary>
+        private AudioListener _audioListener;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _ambientAudioSource = gameObject.AddComponent<AudioSource>();
+            _ambientAudioSource.loop = true;
+
+            AudioEvent.OnAudioEvent += PlaySound;
+            LocationAudioEvent.OnLocationAudioEvent += PlaySoundAtPosition;
+            _audioListener ??= gameObject.AddComponent<AudioListener>();
+            _audioListener.enabled = false;
+        }
+        
+        /// <summary>
+        /// Enable the default <see cref="_audioListener"/>
+        /// </summary>
+        /// <param name="state">The state to set the AudioListener</param>
+        public void EnableDefaultListener(bool state)
+        {
+            if (!_audioListener) return;
+            _audioListener.enabled = state;
+        }
+
+        private void Start()
+        {
+            _audioListener.enabled = false;
+        }
+
+        private void OnDestroy()
+        {
+            AudioEvent.OnAudioEvent -= PlaySound;
+            LocationAudioEvent.OnLocationAudioEvent -= PlaySoundAtPosition;
+        }
+
+        /// <summary>
+        /// Sets the ambient clip and volume.
+        /// Used for setting new clips.
+        /// </summary>
+        /// <remarks> Will stop playing if audioClip is null </remarks>
+        /// <param name="ambientClip">Sound to play</param>
+        /// <param name="volume">Volume from 0.0 - 1.0</param>
+        public void SetAmbientClip(AudioClip audioClip, float volume)
+        {
+            if (!audioClip)
+            {
+                PlayAmbient(false);
+                return;
+            }
+
+            _ambientAudioSource.clip = audioClip;
+            _ambientAudioSource.volume = volume;
+            PlayAmbient(true);
+        }
+
+        /// <summary>
+        /// Method for Start/Stop ambient AudioSource.
+        /// Used for pausing and resuming.
+        /// </summary>
+        /// <param name="state">start or stop</param>
+        public void PlayAmbient(bool state)
+        {
+            if (_ambientAudioSource == null) return;
+
+            if (state && _ambientAudioSource.clip)
+            {
+                _ambientAudioSource?.Play();
+            }
+            else
+            {
+                _ambientAudioSource?.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Play the specified sound.
+        /// </summary>
+        /// <param name="audioData">The sound to play.</param>
+        public void PlaySound(AudioEventData audioData)
+        {
+            if (!audioData?.clip) return;
+
+            // Play the sound.
+            _audioSource?.PlayOneShot(audioData.clip, audioData.volume);
+        }
+        
+        /// <summary>
+        /// Play the specified sound at the specified position.
+        /// </summary>
+        /// <param name="audioData">The audio data to play</param>
+        public void PlaySoundAtPosition(LocationAudioEventData audioData)
+        {
+            if (!audioData?.clip) return;
+
+            // Play the sound at the specified position.
+            AudioSource.PlayClipAtPoint(audioData.clip, audioData.location, audioData.volume); 
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace Utility
+{
+
+    /// <summary>
+    /// Interface for singleton classes.
+    /// </summary>
+    public interface ISingleton
+    {
+        /// <summary>
+        /// Initializes the singleton instance. Ensure it loads into Unity properly.
+        /// </summary>
+        void Initialize();
+    }
+    
+    /// <summary>
+    /// A manager for initializing singletons in Unity.
+    /// </summary>
+    public class SingletonManager : MonoBehaviour
+    {
+        /// <summary>
+        /// Initializes all singletons before the scene is loaded.
+        /// </summary>
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void LoadSingletons()
+        {
+            var singletonTypes = Assembly.GetAssembly(typeof(Singleton<>))
+                .GetTypes()
+                .Where(t => t.BaseType != null &&
+                                    t.BaseType.IsGenericType &&
+                            t.BaseType.GetGenericTypeDefinition() == typeof(Singleton<>));
+
+            foreach (var type in singletonTypes)
+            {
+                var instanceProperty = type.BaseType?.GetProperty(nameof(Singleton<UnityEngine.Component>.Instance));
+                if (instanceProperty == null) continue;
+                
+                // This should trigger the singleton creation
+                var instance = (ISingleton)instanceProperty.GetValue(null);
+                instance?.Initialize();
+            }
+        }
+    }
+    
+    /// <summary>
+    /// A generic singleton class for Unity components.
+    /// </summary>
+    /// <typeparam name="T">The type of the singleton component.</typeparam>
+    public abstract class Singleton<T> : MonoBehaviour, ISingleton where T : UnityEngine.Component
+    {
+        /// <summary>
+        /// If set to <c>true</c>, the object will be unparented on Awake.
+        /// </summary>
+        public bool UnParentOnAwake = true;
+
+        /// <summary>
+        /// The singleton instance.
+        /// </summary>
+        private static T instance;
+
+        /// <summary>
+        /// Gets the singleton instance, creating it if necessary.
+        /// </summary>
+        public static T Instance => instance ??= SingletonGameObject.AddComponent<T>();
+
+        /// <summary>
+        /// Gets the GameObject for the singleton instance, creating it if necessary.
+        /// </summary>
+        private static GameObject SingletonGameObject
+        {
+            get
+            {
+                if (instance != null)
+                {
+                    return instance.gameObject;
+                }
+
+                var go = new GameObject(typeof(T).Name);
+                return go;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance.
+        /// </summary>
+        public virtual void Initialize()
+        {
+            if (!Application.isPlaying)
+            {
+                Debug.LogWarning($"Singleton {typeof(T)} initialized in edit mode.");
+                return;
+            }
+
+            if (UnParentOnAwake)
+            {
+                transform.SetParent(null);
+            }
+
+            if (instance == null || instance == this)
+            {
+                instance = this as T;
+                DontDestroyOnLoad(transform.gameObject);
+                enabled = true;
+            }
+            else if (instance != this)
+            {
+                Debug.LogWarning($"Multiple instances of {typeof(T)} found. Destroying {gameObject.name}.");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
