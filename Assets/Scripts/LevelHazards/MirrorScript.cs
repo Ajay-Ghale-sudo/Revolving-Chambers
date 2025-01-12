7c46be5397393d98d019d732e67fe34143f75d24
Andrew <andrewkarj@gmail.com>
2025-01-12 15:40:40 -0800

    Added diamond boss placeholder model and got it moving/rotating around arena. Added placeholder mirror model and got them moving in opposite direction.

---
+++ Assets/Scripts/LevelHazards/MirrorScript.cs
@@ -0,0 +1,13 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class MirrorScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.DORotate(new Vector3(0f, -360.0f, 0.0f), 30f, RotateMode.FastBeyond360).SetLoops(-1).SetRelative(true).SetEase(Ease.Linear);
+        
+    }
+    
+}
