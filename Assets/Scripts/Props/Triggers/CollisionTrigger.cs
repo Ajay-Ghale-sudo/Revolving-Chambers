d7342138b5fd7937504279520fe2d825506e2be3
Tyler Massicotte <Tyler@pseudocube.com>
2025-01-14 03:32:43 +0000

    Merged PR 198: Game Events
    added event triggers
    - Base Trigger class
    - OnLoadTrigger: Invokes the events when loaded
    - CollisionTrigger: Invokes the events when collided with
    
    refactored game events to have better editor support
    
    Related work items: #345

---
+++ Assets/Scripts/Props/Triggers/CollisionTrigger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Events;
+using UnityEngine;
+
+namespace Props.Triggers
+{
+    /// <summary>
+    /// Trigger that activates when a collider enters the trigger.
+    /// </summary>
+    [RequireComponent(typeof(Collider))]
+    public class CollisionTrigger : TriggerBase 
+    {
+        /// <summary>
+        /// Should the events be invoked with the collider that entered the trigger?
+        /// </summary>
+        [Tooltip("Should the events be invoked with the collider that entered the trigger?")]
+        [SerializeField] private bool invokeWithCollider = false;
+        
+        /// <summary>
+        /// The currently colliding gameobject.
+        /// </summary>
+        private GameObject _currentlyColliding;
+        
+        /// <summary>
+        /// When a collider enters the trigger, invoke the events.
+        /// </summary>
+        /// <param name="other">The Collider colliding with this trigger.</param>
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!string.IsNullOrEmpty(tagFilter) && !other.CompareTag(tagFilter)) return;
+            _currentlyColliding = other.gameObject;
+            InvokeEvents();
+        }
+        
+        /// <summary>
+        /// When a collider exits the trigger, set the currently colliding gameobject to null.
+        /// </summary>
+        /// <param name="other">The Collider leaving this trigger.</param>
+        private void OnTriggerExit(Collider other)
+        {
+            if (!string.IsNullOrEmpty(tagFilter) && !other.CompareTag(tagFilter)) return;
+            if (_currentlyColliding == other.gameObject)
+                _currentlyColliding = null;
+        }
+
+        protected override void InvokeEvents()
+        {
+            if (!invokeWithCollider) base.InvokeEvents();
+            else
+            {
+                foreach (var gameEvent in events)
+                {
+                    gameEvent.Invoke(_currentlyColliding);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Props/Triggers/OnLoadTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Props.Triggers
+{
+    /// <summary>
+    /// Trigger that activates when the object is loaded.
+    /// </summary>
+    public class OnLoadTrigger : TriggerBase 
+    {
+        private void Start()
+        {
+            Trigger();
+        }
+    }
+}
