84c04b0394c7aae6a3ec5752a9806e6e4bb7244c
massicottetyler <tyler@pseudocube.com>
2025-01-14 14:26:26 -0800

    diamond boss code cleanup

--- Assets/Scripts/Boss/DiamondBoss.cs
+++ Assets/Scripts/Boss/DiamondBoss.cs
@@ -10,114 +10,150 @@ using Weapon;
 
 namespace Boss
 {
+    /// <summary>
+    /// Data for the diamond boss attack.
+    /// </summary>
     [Serializable]
     public struct DiamondBossAttackData
     {
+        /// <summary>
+        /// The ammo the attack uses.
+        /// </summary>
         public Ammo ammo;
+        
+        /// <summary>
+        /// The fire rate of the attack.
+        /// </summary>
         public float fireVariation;
+        
+        /// <summary>
+        ///  The amount of cooldown left
+        /// </summary>
         public float cooldown;
     }
-    public class DiamondBoss : MonoBehaviour, IDamageable
+    public class DiamondBoss : Damageable 
     {
 
-        // List of transforms for the projectiles to spawn at.
+        /// <summary>
+        /// The spawn targets for the projectiles. 
+        /// </summary>
         [SerializeField] 
         public List<Transform> projectileSpawnTargets;
+        
+        /// <summary>
+        /// Spawn targets for the diagonal projectiles.
+        /// </summary>
         [SerializeField]
         public List<Transform> projectileDiagSpawnTargets;
         
-        // Projectile attack data.
+        /// <summary>
+        /// The attack data for the boss.
+        /// </summary>
         public DiamondBossAttackData attackData;
 
-        private float maxSpeed = 0.25f;
-        private float currentTime = 0;
-        private bool running;
-        private bool testPattern;
-
-        public AnimationCurve speedCurve;
+        /// <summary>
+        /// Whether the boss is currently running an attack pattern.
+        /// </summary>
+        private bool _running;
         
-        // Since spline animate is being annoying we'll write our own.
+        /// <summary>
+        /// Controls which attack pattern the boss is using.
+        /// </summary>
+        private bool _useAltAttackPattern;
+
+        /// <summary>
+        /// The spline for the boss to follow.
+        /// </summary>
         [SerializeField]
         public SplineContainer spline;
+        
+        /// <summary>
+        /// Cached length of the current spline
+        /// </summary>
+        private float _splineLength = 0f;
+        
+        /// <summary>
+        /// The movement speed of the boss.
+        /// </summary>
         public float moveSpeed = 1f;
-        private float currentDistance = 0f;
-        private float movement = 0f;
+        
+        /// <summary>
+        /// The current distance along the spline.
+        /// </summary>
+        private float _currentDistance = 0f;
 
+        
+        /// <summary>
+        /// Time between boss attacks.
+        /// </summary>
+        [SerializeField] private float attackInterval = 2f;
+
+        /// <summary>
+        /// The rotation vector for the boss.
+        /// </summary>
+        [SerializeField] private Vector3 rotationVector = new Vector3(0f, 360f, 0f);
+
+        /// <summary>
+        /// The duration of the rotation. Shorter duration, faster spin.
+        /// </summary>
+        [SerializeField] private float rotationDuration = 1f;
+        
+        /// <summary>
+        /// The mode of rotation for the boss.
+        /// </summary>
+        [SerializeField] private RotateMode rotateMode = RotateMode.FastBeyond360;
+
+        
         void Start()
         {
-            transform.DORotate
-                    (new Vector3(0f, 360.0f, 0.0f), 1f, RotateMode.FastBeyond360)
+            transform.DORotate(rotationVector, rotationDuration, rotateMode)
                 .SetLoops(-1).SetRelative(true).SetEase(Ease.Linear);
             StartCoroutine(AttackPatternCoroutine());
+
+            _splineLength = spline.CalculateLength();
 
         }
         void Update()
         {
             // Calculate the target position on the spline.
             // Sets the world position for the player to move to calculated by the normalized value currentDistance.
-            Vector3 targetPosition = spline.EvaluatePosition(currentDistance);
+            Vector3 targetPosition = spline.EvaluatePosition(_currentDistance);
             
             // Move the character towards the target position on the spline.
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
             
-            // If the end of the spline is reached, go back to the beginning.
-            if (currentDistance >= 1f)
-            {
-                currentDistance = 0f;
-            }
-            else
-            {
-                // Adjust the movement based on the length of the spline
-                float splineLength = spline.CalculateLength();
-                float movement = moveSpeed * Time.deltaTime / splineLength;
-                currentDistance += movement;
-            }
+            // calculate how far along we are 0 -> 1.0
+            _currentDistance = (_currentDistance + ((moveSpeed * Time.deltaTime) / _splineLength)) % 1f;
         }
 
+        /// <summary>
+        /// Coroutine for the attack pattern.
+        /// </summary>
+        /// <returns></returns>
         IEnumerator AttackPatternCoroutine()
         {
-            running = true;
-            while (running)
+            _running = true;
+            while (_running)
             {
                 SpawnBulletPattern();
-                testPattern = !testPattern;
+                _useAltAttackPattern = !_useAltAttackPattern;
                 Invoke(nameof(SpawnBulletPattern), .5f);
-                yield return new WaitForSeconds(2);
+                yield return new WaitForSeconds(attackInterval);
             }
         }
 
+        /// <summary>
+        /// Spawns the bullet pattern for the current attack pattern.
+        /// </summary>
         void SpawnBulletPattern()
         {
-            if (testPattern)
+            var targets = _useAltAttackPattern ? projectileSpawnTargets : projectileDiagSpawnTargets;
+            foreach (var target in targets)
             {
-                foreach (Transform projectileSpawnTarget in projectileSpawnTargets)
-                {
-                    var spawnPosition = projectileSpawnTarget.position;
-                    var direction = projectileSpawnTarget.position - transform.position;
-                    var projectile = BulletManager.Instance.SpawnBullet(attackData.ammo, spawnPosition, Quaternion.LookRotation(direction));
-                }
+                var spawnPosition = target.position;
+                var direction = target.position - transform.position;
+                var projectile = BulletManager.Instance.SpawnBullet(attackData.ammo, spawnPosition, Quaternion.LookRotation(direction));
             }
-
-            if (!testPattern)
-            {
-                foreach (Transform projectileDiagSpawnTarget in projectileDiagSpawnTargets)
-                {
-                    var spawnPosition = projectileDiagSpawnTarget.position;
-                    var direction = projectileDiagSpawnTarget.position - transform.position;
-                    var projectile = BulletManager.Instance.SpawnBullet(attackData.ammo, spawnPosition, Quaternion.LookRotation(direction));
-                }
-            }
-        }
-        public UnityEvent OnDamage { get; } = new();
-        public UnityEvent OnDeath { get; } = new();
-        public void TakeDamage(DamageData damage)
-        {
-            
-        }
-
-        public void PlayDamageEffect(Color colour)
-        {
-            
         }
     }
 }
