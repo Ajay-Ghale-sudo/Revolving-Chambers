9d0cd7a1ef09d8a14957c3c7675eeb2c95300884
Vincent <vince.huang123@gmail.com>
2025-01-20 02:46:23 +0000

    Merged PR 221: Added dashing animation
    Added dashing animation
    
    Related work items: #349

---
+++ Assets/Scripts/Player/CharacterAnimationHandler.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CharacterAnimationHandler : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    public void SetMoveDirection()
+    {
+
+    }
+
+    public void Play_Dash()
+    {
+        if (_animator == null) return;
+
+    }
+}
